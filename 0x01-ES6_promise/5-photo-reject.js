24fa816b2ff95bf22baa977df6042b3cf1105826
C4Confidence <iamconfidence5@gmail.com>
2024-07-24 22:41:31 +0100

    Reject the promises

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,4 @@
+// a a function that returns a promise rejecting with an Error.
+export default function uploadPhoto(filename) {
+  return Promise.reject(Error(`${filename} cannot be processed`));
+}
