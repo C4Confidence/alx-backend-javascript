581a8350c139053fcbea01418629348ec31b6d2f
C4Confidence <iamconfidence5@gmail.com>
2024-07-30 14:31:52 +0100

    Updated Grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,16 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
-    return list
-      .filter((obj) => obj.location === city)
-      .map((student) => {
-        // Find the grade object for the current student
-        const gradeObj = newGrade.find((studentGrade) => studentGrade.studentId === student.id);
-        
-        // Create a new student object with the updated grade
-        const updatedStudent = {
-          ...student,
-          grade: gradeObj ? gradeObj.grade : 'N/A',
-        };
-  
-        return updatedStudent;
-      });
-  }
-  
+  return list
+    .filter((obj) => obj.location === city)
+    .map((student) => {
+      // Find the grade object for the current student
+      const gradeObj = newGrade.find((studentGrade) => studentGrade.studentId === student.id);
+
+      // Create a new student object with the updated grade
+      const updatedStudent = {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+
+      return updatedStudent;
+    });
+}
