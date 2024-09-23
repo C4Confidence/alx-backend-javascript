0c2626d6ab5a22343f13d710e89ed1d5f22e2755
C4Confidence <iamconfidence5@gmail.com>
2024-09-23 16:17:23 +0100

    Using

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,7 +7,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim(); // Convert buffer to string
   process.stdout.write(`Your name is: ${name}\n`);
+
+  // End program with closing message
   process.stdout.write('This important software is now closing\n');
   process.exit(); // Exit the program
 });
-
